c285bec7125a28bc5bf13a82cc16aae2b0ccfcdf
DHRUVINKUMAR BHATT <75667686+dhruvinkumar@users.noreply.github.com>
2023-03-15 21:49:14 +0530

    Add files via upload

---
+++ If_Else.py
@@ -0,0 +1,14 @@
+a = int(input("Enter your age "))
+print("Your age is:",a)
+# Conditional operators
+# >, <, >=, <=, ==
+print(a>18)50
+
+print(a<18)
+print(a>=18)
+print(a<=18)
+print(a==18)
+if(a>18):
+    print("You are eligible to vote")
+else:
+    print("You are NOT eligible to vote")
