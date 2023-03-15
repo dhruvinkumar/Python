94323d21bbb564311c1e092babfb95bdfaa24347
DHRUVINKUMAR BHATT <75667686+dhruvinkumar@users.noreply.github.com>
2023-03-15 21:49:41 +0530

    Add files via upload

---
+++ Taking_User_input.py
@@ -0,0 +1,7 @@
+# a=input("Enter your name ")
+# print("My name is",a)
+
+# Python by default take input as string
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print(x + y)
