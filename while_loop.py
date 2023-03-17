3fd8d0c5d9570516f36e0d39a3914c2cb91ba5d7
DHRUVINKUMAR BHATT <75667686+dhruvinkumar@users.noreply.github.com>
2023-03-17 21:37:01 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,19 @@
+# i=0
+# while i <= 3:
+#     print(i)
+#     i+=1
+
+# print("Done with the loop")
+
+# i = int(input("Enter the number: "))
+# while(i != -1):
+#     print(i)
+#     i = int(input("Enter the number: "))
+# print("Done with the loop")
+
+count = 5
+while (count > 0):
+    print(count)
+    count-=1
+else:              # With while loop we can use else also if it does not go inside then it will go into else block
+    print("number is 0 now!!!")
